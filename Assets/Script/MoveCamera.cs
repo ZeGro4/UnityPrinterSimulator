f8b04a1cc66ee16e71091834d51e6c4d8696fdc8
ZeGro4 <dased.art.16@mail.ru>
2025-11-11 18:50:10 +0300

    added start settings and model for project

---
+++ Assets/Script/MoveCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    // Чувствительность мыши
+    public float mouseSensitivity = 100f;
+
+    // Ссылка на Transform всего персонажа
+    public Transform playerBody;
+
+    // Переменная для хранения вращения по оси X (вертикальное вращение)
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        // Блокируем курсор в центре экрана, чтобы он не мешал
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Получаем ввод от мыши
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Уменьшаем xRotation на значение движения мыши по Y
+        // (движение вверх-вниз)
+        xRotation -= mouseY;
+
+        // Ограничиваем вращение по вертикали, чтобы игрок не мог "сломать шею"
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Применяем вращение к камере по оси X (вертикально)
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Вращаем всего персонажа по оси Y (горизонтально)
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Ссылка на компонент CharacterController
+    private CharacterController controller;
+
+    // Скорость передвижения персонажа
+    public float speed = 12f;
+
+    // Гравитация
+    public float gravity = -9.81f;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        // Получаем компонент CharacterController при старте
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Debug.Log("Moving...");
+        // Проверяем, стоит ли персонаж на земле
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            // Сбрасываем вертикальную скорость, чтобы персонаж не "утонул" под землю
+            velocity.y = -2f;
+        }
+
+        // Получаем ввод с клавиатуры (оси "Horizontal" и "Vertical")
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+
+        // Создаем вектор движения на основе направления, куда смотрит персонаж
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Перемещаем персонажа с помощью CharacterController
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Применяем гравитацию
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
