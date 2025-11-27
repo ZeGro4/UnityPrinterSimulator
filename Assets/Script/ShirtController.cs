22bebf58f12cd2240055b857c63bad2de656bac9
ZeGro4 <dased.art.16@mail.ru>
2025-11-27 20:55:00 +0300

    added shirt

---
+++ Assets/Script/ShirtController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShirtController : MonoBehaviour {
+
+	Animator animator;
+	public Material material;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (animator != null)
+		{
+			if (Input.GetKeyDown(KeyCode.J))
+			{
+				animator.SetBool("IsMoving", true);
+				StartCoroutine(ColorShirt());
+			}
+			
+		}
+
+	}
+
+	private IEnumerator ColorShirt()
+	{
+		yield return new WaitForSeconds(6f);
+		gameObject.GetComponent<MeshRenderer>().material = material;
+	}
+}
